6bf8791353a96d2c8cb6f9fd8efaaec6a271bf34
Joelkons02 <konsenj02@gmail.com>
2023-07-08 17:25:10 -0700

    inint

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,29 @@
-from fabric.api import local
+#!/usr/bin/python3
+"""A module for Fabric script that generates a .tgz archive."""
+import os
 from datetime import datetime
+from fabric.api import local, runs_once
 
+
+@runs_once
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
     try:
-        # Create the folder versions if it doesn't exist
-        local("mkdir -p versions")
-
-        # Create the name of the archive
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_name = "web_static_" + now + ".tgz"
-
-        # Create the archive
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-
-        # Return the path of the archive if it was created successfully
-        return "versions/{}".format(archive_name)
-    except:
-        # Return None if an exception occurred
-        return None
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
